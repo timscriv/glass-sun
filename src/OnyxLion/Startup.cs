5bde18104511d9bea35998248a782c61e0855a9e
Tim Scrivner <tscrivner@ethosgroup.com>
2017-11-20 12:14:51 -0600

    add swagger to onyx lion

--- src/OnyxLion/Startup.cs
+++ src/OnyxLion/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace OnyxLion
 {
@@ -24,6 +25,11 @@ namespace OnyxLion
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Onyx Lion", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,12 @@ namespace OnyxLion
             }
 
             app.UseMvc();
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Glass Sun API");
+                });
         }
     }
 }
